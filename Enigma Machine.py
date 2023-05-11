3adf0fd805f7c76e1522ab1e27ca950302b9675f
Patrickc <darkxwarrior13@gmail.com>
2023-05-11 11:13:15 -0700

    encryption stable - enigma machine

--- Enigma Machine.py
+++ Enigma Machine.py
@@ -10,7 +10,8 @@ class RotorWheel:
         self.rotor3_position = rotor3_position
 
 
-Rotors = RotorWheel(1, 1, 1)
+Rotors = RotorWheel(14, 18, 2)
+
 
 def translate(phrase):
     plug_board_conversions = {
@@ -96,49 +97,41 @@ def translate(phrase):
         "x": 24,
         "y": 25,
         "z": 26, }
-    translation = ""
-    rotor1_translation = ""
-    rotor2_translation = ""
-    rotor3_translation = ""
-    rotor1_translation2 = ""
-    rotor2_translation2 = ""
-    rotor3_translation2 = ""
-    rotor1_changing_rotor2_position = 0
     rotor2_changing_rotor3_position = 0
     rotor1_starting_point = copy.copy(Rotors.rotor1_position)
-    rotor2_starting_point = copy.copy(Rotors.rotor2_position)
     final_translation = []
 
     for letter in phrase:
-        translation = translation + plug_board_conversions.get(letter)
-        translation2 = translating_to_numbers.get(letter)
+        translation = plug_board_conversions.get(letter)
+        translation2 = translating_to_numbers.get(translation)
         # 1st rotor wheel
         if (Rotors.rotor1_position + translation2) > 26:
-            translation3 = Rotors.rotor1_position + translation2 - 26
+            translation3 = (Rotors.rotor1_position + translation2) - 26
         else:
             translation3 = Rotors.rotor1_position + translation2
+
         Rotors.rotor1_position += 1
-        if Rotors.rotor1_position == 27:
-            Rotors.rotor1_position = 1
+        if Rotors.rotor1_position > 26:
+            Rotors.rotor1_position = Rotors.rotor1_position - 26
         if Rotors.rotor1_position == rotor1_starting_point:
             Rotors.rotor2_position += 1
 
         # 2nd rotor wheel
         if (Rotors.rotor2_position + translation3) > 26:
-            translation4 = Rotors.rotor2_position + translation3 - 26
+            translation4 = (Rotors.rotor2_position + translation3) - 26
         else:
             translation4 = Rotors.rotor2_position + translation3
-        if Rotors.rotor2_position == 27:
-            Rotors.rotor2_position = 1
+        if Rotors.rotor2_position > 26:
+            Rotors.rotor2_position = Rotors.rotor2_position - 26
         rotor2_changing_rotor3_position += 1
         if isdigit(rotor2_changing_rotor3_position / 26):
             Rotors.rotor3_position += 1
-        if Rotors.rotor3_position == 27:
+        if Rotors.rotor3_position > 26:
             Rotors.rotor3_position = 1
 
         # 3rd rotor wheel
-        if (Rotors.rotor3_position + translation3) > 26:
-            translation5 = Rotors.rotor3_position + translation4 - 26
+        if (Rotors.rotor3_position + translation4) > 26:
+            translation5 = (Rotors.rotor3_position + translation4) - 26
         else:
             translation5 = Rotors.rotor3_position + translation4
 
@@ -160,22 +153,22 @@ def translate(phrase):
 
         # 3rd wheel 2nd time
         if (Rotors.rotor3_position + translation5) > 26:
-            translation6 = Rotors.rotor3_position + translation5 - 26
+            translation6 = (Rotors.rotor3_position + translation5) - 26
         else:
             translation6 = Rotors.rotor3_position + translation5
 
         # 2nd wheel 2nd time
         if (Rotors.rotor2_position + translation6) > 26:
-            translation7 = Rotors.rotor2_position + translation6 - 26
+            translation7 = (Rotors.rotor2_position + translation6) - 26
         else:
             translation7 = Rotors.rotor2_position + translation6
 
         # 1st wheel 2nd time
         if (Rotors.rotor1_position + translation7) > 26:
-            translation8 = Rotors.rotor1_position + translation7 - 26
+            translation8 = (Rotors.rotor1_position + translation7) - 26
         else:
             translation8 = Rotors.rotor1_position + translation7
-
+        print(translation8)
         final_translation.append(rotor_possible_positions.get(translation8))
 
     return "".join(str(e) for e in final_translation)
